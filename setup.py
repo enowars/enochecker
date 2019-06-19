f2f05074d4f8627af9b9a22447d7c8fa4725c3b2
M-Munier <marc@geomaps.de>
2019-06-19 13:34:14 +0200

    inc version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker",
-    version="0.0.4",
+    version="0.0.5",
     author="domenukk",
     author_email="dmaier@sect.tu-berlin.de",
     description="Library to build checker scripts for EnoEngine A/D CTF Framework in Python",
---
+++ src/enochecker/longrunningTasks.py
@@ -0,0 +1,46 @@
+from abc import ABC
+
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from starlette.background import BackgroundTask
+
+from functools import partial
+
+
+class LR_Action(ABC):
+
+    @abstractmethod
+    async def initial_call(**kwargs):
+        pass
+    
+    @abstractmethod
+    async def background_call():
+        pass
+
+
+
+class LRConnHandler(Starlette):
+    def __init__(self, lr_callables, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for call in lr_callables:
+
+            self.add_route(
+                f"/{call.__class__.__name__}",
+                partial(self._callwrapper, lr_call=call),
+                methods=["POST"]
+                )
+
+    async def _callwrapper(lr_call=None):
+        kwargs = await request.json()
+        
+        # initial Call
+        (ret_dict, stuff) = await lr_call.initial_call(kwargs)
+
+        # Background Task
+        task = BackgroundTask(lr_call.background_call, stuff)
+        return JSONResponse(ret_dict, task)
+
+
+
