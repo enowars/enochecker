24af5135d2ebcc16cf96e855bd98d109cfb35ab8
M-Munier <marc@geomaps.de>
2019-06-20 17:31:16 +0200

    name inconsistancy fixed

--- src/enochecker/checkerservice.py
+++ src/enochecker/checkerservice.py
@@ -213,7 +213,7 @@ def checker_routes(checker_cls):
             
             kwargs = assert_types(req_json, spec)
 
-            checker = checker_cls(request_params=kwargs, **kwargs)
+            checker = checker_cls(request_dict=kwargs, **kwargs)
     
             checker.logger.info(request.json)
             res = checker.run().name
