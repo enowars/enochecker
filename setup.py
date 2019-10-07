fc736889342723bfc6691a8741f1824477e48848
Benedikt Constantin Radtke <benediktradtke@mailbox.tu-berlin.de>
2019-10-07 23:52:51 +0200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-import os
 import setuptools
-from io import open  # for Python 2 (identical to builtin in Python 3)
-
-repo_root = os.path.abspath(os.path.dirname(__file__))
-# src = os.path.join(repo_root, "src")
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -18,7 +13,7 @@ setuptools.setup(
     author_email="dmaier@sect.tu-berlin.de",
     description="Library to build checker scripts for EnoEngine A/D CTF Framework in Python",
     long_description=long_description,
-    url="https://github.com/domenukk/enochecker",
+    url="https://github.com/ENOWARS/enochecker",
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
     install_requires=requirements,
@@ -35,11 +30,6 @@ setuptools.setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
 
