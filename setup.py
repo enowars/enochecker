624fbe2c61e7e57911d5e7249eda7192a350f7da
Benedikt Radtke <Trolldemorted@users.noreply.github.com>
2020-03-01 10:54:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker",
-    version="0.0.6",
+    version="0.0.8",
     author="domenukk",
     author_email="dmaier@sect.tu-berlin.de",
     description="Library to build checker scripts for EnoEngine A/D CTF Framework in Python",
