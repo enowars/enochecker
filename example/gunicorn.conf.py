0fbc55f357442bb4f7948a76041f8001f82497b8
MMunier <m.munier@campus.tu-berlin.de>
2021-05-26 17:44:57 +0200

    Update gunicorn.conf.py

--- example/gunicorn.conf.py
+++ example/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 worker_class = "eventlet"
-workers = multiprocessing.cpu_count()*2+1
+workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
 timeout = 90
 keepalive = 3600
