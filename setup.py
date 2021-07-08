f05d55371e5da3d49b1150ea73019adf6465a33d
ldruschk <14059613+ldruschk@users.noreply.github.com>
2021-07-08 21:16:00 +0200

    bump version to 0.5.0 (#133)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="enochecker",
-    version="0.4.2",
+    version="0.5.0",
     author="domenukk",
     author_email="dmaier@sect.tu-berlin.de",
     description="Library to build checker scripts for EnoEngine A/D CTF Framework in Python",
